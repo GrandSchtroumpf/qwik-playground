7bae42b0c8bdd5f709f514fbe1a5626cd6e526da
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2023-06-10 01:31:51 +0200

    add viewport meta tag

--- src/root.tsx
+++ src/root.tsx
@@ -16,6 +16,7 @@ export default component$(() => {
       <head>
         <meta charSet="utf-8" />
         <link rel="manifest" href="/manifest.json" />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0"></meta>
       </head>
       <body lang="en">
         <RouterOutlet />
