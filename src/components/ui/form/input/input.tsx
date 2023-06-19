e49e7a5dbc457f9285331bcb21e7bcef40c91e12
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2023-06-19 17:54:36 +0200

    remove useForm from input

--- src/components/ui/form/input/input.tsx
+++ src/components/ui/form/input/input.tsx
@@ -2,7 +2,6 @@ import type { QwikJSX} from "@builder.io/qwik";
 import { useSignal } from "@builder.io/qwik";
 import { component$, Slot, useContext, useStyles$ } from "@builder.io/qwik";
 import type { FieldProps} from "../field";
-import { useFormController } from "../form";
 import { FormFieldContext } from "../form-field/form-field";
 import styles from './input.scss?inline';
 
@@ -15,7 +14,6 @@ export const Input = component$((props: InputProps) => {
   useStyles$(styles);
   const { id } = useContext(FormFieldContext);
   const ref = useSignal<HTMLInputElement>();
-  useFormController(ref);
 
   return <div class="field">
     <Slot name="prefix"/>
